0113a2b7761e20dd896ff146f1d7591b02d5c2d5
tobbaumann <tobbaumann@gmail.com>
2012-11-24 15:03:13 +0100

    license

--- org.baumato.worktracker.core/src/org/baumato/wt/core/PersistenceService.java
+++ org.baumato.worktracker.core/src/org/baumato/wt/core/PersistenceService.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 tobbaumann.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     tobbaumann - initial API and implementation
+ ******************************************************************************/
 package org.baumato.wt.core;
 
 import java.util.List;
