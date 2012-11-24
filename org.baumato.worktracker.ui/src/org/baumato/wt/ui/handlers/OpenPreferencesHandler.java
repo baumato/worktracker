590518e91746224e23fc916201c628a4fe6126f0
tobbaumann <tobbaumann@gmail.com>
2012-11-24 15:03:52 +0100

    handlers

---
+++ org.baumato.worktracker.ui/src/org/baumato/wt/ui/handlers/OpenPreferencesHandler.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 tobbaumann.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     tobbaumann - initial API and implementation
+ ******************************************************************************/
+package org.baumato.wt.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class OpenPreferencesHandler {
+
+	@Execute
+	public void openPreferences(Shell shell) {
+		MessageDialog.openInformation(shell, "Preferences", "Open Preferences");
+	}
+}
