b43a0503cb490cc97a7a6ead84bc5b7eb60eb614
tobbaumann <tobbaumann@gmail.com>
2013-02-24 00:27:51 +0100

    licence added

--- org.baumato.worktracker.ui/src/org/tobbaumann/wt/ui/views/DescriptionsView.java
+++ org.baumato.worktracker.ui/src/org/tobbaumann/wt/ui/views/DescriptionsView.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012 tobbaumann.
+ * Copyright (c) 2013 Tobias Baumann.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     tobbaumann - initial API and implementation
+ *     Tobias Baumann - initial API and implementation
  ******************************************************************************/
 package org.tobbaumann.wt.ui.views;
 
