1fb6107abe8206ab6b63485f17d450b06469e862
tobbaumann <tobbaumann@gmail.com>
2013-10-26 18:20:48 +0200

    Apply SWT.SHORT style.

--- org.tobbaumann.worktracker.ui/src/org/tobbaumann/wt/ui/views/workitem/TimeCellEditor.java
+++ org.tobbaumann.worktracker.ui/src/org/tobbaumann/wt/ui/views/workitem/TimeCellEditor.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Tobias Baumann - initial API and implementation
  ******************************************************************************/
@@ -51,7 +51,7 @@ public class TimeCellEditor extends CellEditor {
 	}
 
 	private int applyStyle() {
-		return getStyle() | SWT.TIME;
+		return getStyle() | SWT.TIME | SWT.SHORT;
 	}
 
 	private void applyListeners() {
