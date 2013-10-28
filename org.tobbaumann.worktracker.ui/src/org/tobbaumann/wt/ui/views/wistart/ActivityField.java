c00d940dcb7fc5aab55f4c997e0adf0ef99891fd
tobbaumann <tobbaumann@gmail.com>
2013-10-28 19:57:22 +0100

    Refactor

--- org.tobbaumann.worktracker.ui/src/org/tobbaumann/wt/ui/views/wistart/ActivityField.java
+++ org.tobbaumann.worktracker.ui/src/org/tobbaumann/wt/ui/views/wistart/ActivityField.java
@@ -1,15 +1,6 @@
 package org.tobbaumann.wt.ui.views.wistart;
 
-import static com.google.common.collect.Lists.newArrayList;
-
-import java.util.Collection;
-
-import org.eclipse.core.databinding.observable.list.IListChangeListener;
-import org.eclipse.core.databinding.observable.list.IObservableList;
-import org.eclipse.core.databinding.observable.list.ListChangeEvent;
 import org.eclipse.jface.fieldassist.ContentProposalAdapter;
-import org.eclipse.jface.fieldassist.SimpleContentProposalProvider;
-import org.eclipse.jface.fieldassist.TextContentAdapter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.KeyListener;
@@ -18,7 +9,6 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.tobbaumann.wt.core.WorkTrackingService;
-import org.tobbaumann.wt.domain.Activity;
 
 /**
  *
@@ -28,7 +18,7 @@ import org.tobbaumann.wt.domain.Activity;
 public class ActivityField extends Composite {
 
 	private Text txtActivity;
-	private ContentProposalAdapter activiyContentProposalAdapter;
+	private ContentProposalAdapter activityContentProposalAdapter;
 	private WorkTrackingService service;
 
 	/**
@@ -45,42 +35,7 @@ public class ActivityField extends Composite {
 
 	private void createContent() {
 		txtActivity = new Text(this, SWT.SINGLE | SWT.LEAD | SWT.BORDER);
-		applyContentAssist(txtActivity);
-	}
-
-	private void applyContentAssist(Text txtActivity) {
-		SimpleContentProposalProvider proposalProvider = new SimpleContentProposalProvider(
-				createProposalsFromActivities(service.getActivities()));
-		activiyContentProposalAdapter = new ContentProposalAdapter(
-			txtActivity,
-			new TextContentAdapter(),
-			proposalProvider,
-			null,
-			null);
-		proposalProvider.setFiltering(true);
-		activiyContentProposalAdapter.setProposalAcceptanceStyle(ContentProposalAdapter.PROPOSAL_REPLACE);
-		updateProposalsOnActivityListChange(proposalProvider);
-	}
-
-	private String[] createProposalsFromActivities(IObservableList<Activity> activities) {
-		Collection<String> activityNames = newArrayList();
-		for (Object o : activities) {
-			Activity a = (Activity) o;
-			activityNames.add(a.getName());
-		}
-		String[] arrActivityNames = activityNames.toArray(new String[0]);
-		return arrActivityNames;
-	}
-
-	private void updateProposalsOnActivityListChange(final SimpleContentProposalProvider proposalProvider) {
-		final IObservableList<Activity> activities = service.getActivities();
-		activities.addListChangeListener(new IListChangeListener<Activity>() {
-			@Override
-			public void handleListChange(ListChangeEvent<Activity> event) {
-				String[] arrActivityNames = createProposalsFromActivities(activities);
-				proposalProvider.setProposals(arrActivityNames);
-			}
-		});
+		activityContentProposalAdapter = ActivityContentProposalAdapter.applyContentAssist(txtActivity, service);
 	}
 
 	public String getText() {
@@ -106,7 +61,7 @@ public class ActivityField extends Composite {
 	}
 
 	public boolean isProposalPopupOpen() {
-		return activiyContentProposalAdapter != null && activiyContentProposalAdapter.isProposalPopupOpen();
+		return activityContentProposalAdapter != null && activityContentProposalAdapter.isProposalPopupOpen();
 	}
 
 	@Override
